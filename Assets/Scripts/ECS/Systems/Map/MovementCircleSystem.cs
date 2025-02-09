c41a03120c92855a23fa0b93b86959210b56f37c
DESKTOP-5SPN32R\wonst <wonstorm0511>
2025-02-09 22:01:21 +0300

    no message

--- Assets/Scripts/ECS/Systems/Map/MovementCircleSystem.cs
+++ Assets/Scripts/ECS/Systems/Map/MovementCircleSystem.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 namespace Client {
     sealed class MovementCircleSystem : IEcsRunSystem {
-        readonly EcsFilterInject<Inc<PlayerComponent, AreaWalkingComponent>> _filterPlayer;
-        readonly EcsPoolInject<CircleTransformComponent> _transformPool;
+        readonly EcsFilterInject<Inc<PlayerComponent, TransformComponent>> _filterPlayer;
+        readonly EcsPoolInject<TransformComponent> _transformPool;
+        readonly EcsPoolInject<CircleTransformComponent> _transformCirclePool;
         readonly EcsFilterInject<Inc<CircleTransformComponent>> _filterMap;
 
         public void Run (IEcsSystems systems) {
             
             foreach (var entity in _filterMap.Value)
             {
-                ref var transformComp=ref _transformPool.Value.Get(entity);
-                transformComp.Transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                ref var transformComp=ref _transformCirclePool.Value.Get(entity);
+                transformComp.TransformCursor.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                 var worldPositionClick = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                transformComp.Transform.position = GameState.Instance.GetNewPoint(worldPositionClick).PointToWorld;
+                transformComp.TransformCursor.position = GameState.Instance.GetNewPoint(worldPositionClick).PointToWorld;
+                foreach (var entityPlayer in _filterPlayer.Value)
+                {
+                    ref var transformPlayer = ref _transformPool.Value.Get(entityPlayer);
+                    transformComp.TransformForPlayer.position = transformPlayer.Transform.position;
+                }
             }
+            
         }
     }
 }
